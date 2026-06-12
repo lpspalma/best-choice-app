6d3208cb5f88d810d2ca50b0c0e00f2919bc0b93
Lucas Palma <lukspalm@hotmail.com>
2026-06-12 15:16:45 -0300

    Game page refactoring

--- frontend/src/pages/Results.tsx
+++
@@ -1,5 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export function Results() {
-  return <Navigate to="/games" replace />;
-}
--- frontend/src/pages/TodayGames.tsx
+++
@@ -1,5 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export function TodayGames() {
-  return <Navigate to="/games" replace />;
-}
